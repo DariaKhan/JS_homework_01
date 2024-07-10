b56e27115621941961a52c7f01fea2e957564bae
Daria_Khan <tesurus2005@yahoo.com>
2024-07-10 12:38:51 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Задание 1
+
+let gradesCelsius = 24;
+let gradesFahrenheit = gradesCelsius*(9/5)+32;
+console.log(`Температура в градусах Цельсия: ${gradesCelsius} °C`);
+console.log(`Температура в градусах Фаренгейта: ${gradesFahrenheit} °F`);
+
+// Задание 2
+
+let name = 'Дарья';
+let admin = name;
+console.log(`Значение переменной admin: ${admin}`);
